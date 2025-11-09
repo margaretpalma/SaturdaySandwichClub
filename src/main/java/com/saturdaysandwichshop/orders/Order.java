6f1942388306bc0c3b4c5588f589cccf5ebb8414
margaretpalma <mpalma1496@gmail.com>
2025-11-09 14:26:38 -0500

    order screen

--- src/main/java/com/saturdaysandwichshop/orders/Order.java
+++ src/main/java/com/saturdaysandwichshop/orders/Order.java
@@ -4,6 +4,7 @@ import com.saturdaysandwichshop.models.ProductMain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Order {
     //customer order
@@ -18,20 +19,23 @@ public class Order {
         item.add(item);
     }
 
+    //receipt and ui display items
+    public List<ProductMain> getItems(){
+        return new ArrayList<>(items);
+    }
 
     //total cost
-
-
     //streams
+    public double getTotal(){
+        return items.stream().mapToDouble(ProductMain::getPrice).sum();
+    }
 
+    //formatting
+    @Override
+    public String toString() {
+        String itemList = items.isEmpty() ? "No items!" : items.stream().map(items -> String.format(items.getProductName(),
+                items.getPrice())).collect(Collectors.joining("\n"));
 
-
-
-
-
-
-
-
-
-
+        return  itemList + "---\n$Your Total Is" + getTotal();
+    }
 }
