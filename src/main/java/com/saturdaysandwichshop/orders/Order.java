509f966a84e67429e1e892a6c415eda55a27d286
margaretpalma <mpalma1496@gmail.com>
2025-11-09 14:15:27 -0500

    order/home screen

--- src/main/java/com/saturdaysandwichshop/orders/Order.java
+++ src/main/java/com/saturdaysandwichshop/orders/Order.java
@@ -1,11 +1,27 @@
 package com.saturdaysandwichshop.orders;
 
-public class Order {
+import com.saturdaysandwichshop.models.ProductMain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
     //customer order
+
     //arrayList of products
+    //sandwich, chips, drinks
+    private final List<ProductMain> items = new ArrayList<>();
+
+
     //add products
+    public void addItem(ProductMain item){
+        item.add(item);
+    }
+
+
     //total cost
+
+
     //streams
 
 
--- src/main/java/com/saturdaysandwichshop/ui/HomeScreen.java
+++ src/main/java/com/saturdaysandwichshop/ui/HomeScreen.java
@@ -1,4 +1,30 @@
 package com.saturdaysandwichshop.ui;
 
+import com.saturdaysandwichshop.utilities.ConsoleHelper;
+
 public class HomeScreen {
+    //main screen
+
+    //display menu loops, runs until user chooses to exit
+    public void menuDisplay(){
+
+        while(true){
+            System.out.println("Welcome to Saturday Sandwich Shop");
+
+            System.out.println("1) New Order ");
+
+            System.out.println("0) Exit");
+
+            int choice = ConsoleHelper.promptForInt("Enter Option Here (1/0)");
+
+            switch (choice){
+                case 1 ->  new OrderScreen().dispalyOrderMenu();
+                case 0 -> {
+                    System.out.println("See you next Saturday!");
+                    return;
+                }
+                System.out.println("Please Choose 1 or 0");
+            }
+        }
+    }
 }
