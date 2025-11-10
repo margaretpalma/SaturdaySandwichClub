9aa8d72e10cb6342418ae5a5c96dc69722160f98
margaretpalma <mpalma1496@gmail.com>
2025-11-10 12:56:06 -0500

    home screen

--- src/main/java/com/saturdaysandwichshop/ui/HomeScreen.java
+++ src/main/java/com/saturdaysandwichshop/ui/HomeScreen.java
@@ -17,13 +17,13 @@ public class HomeScreen {
 
             int choice = ConsoleHelper.promptForInt("Enter Option Here (1/0)");
 
-            switch (choice) {
-                case 1 -> new OrderScreen().displayOrderScreen();
-                case 0 -> {
-                    System.out.println("See you next Saturday!");
-                    return;
+//            switch (choice) {
+//                case 1 -> new OrderScreen().displayOrderScreen();
+//                case 0 -> {
+//                    System.out.println("See you next Saturday!");
+//                    return;
+
+
                 }
             }
         }
-    }
-}
