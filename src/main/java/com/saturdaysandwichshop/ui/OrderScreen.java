eb8431d1d385c081515cc75e802fc917401688a5
margaretpalma <mpalma1496@gmail.com>
2025-11-10 10:33:46 -0500

    adddrink

--- src/main/java/com/saturdaysandwichshop/ui/OrderScreen.java
+++ src/main/java/com/saturdaysandwichshop/ui/OrderScreen.java
@@ -12,7 +12,7 @@ public class OrderScreen {
 
     private final Order order = new Order();
 
-//main menu
+    //main menu
     public void displayOrderMenu() {
 
         while (true) {
@@ -23,56 +23,56 @@ public class OrderScreen {
 //            System.out.println - checkout
 //            System.out.println - cancel order - back to homepage
 //
-        int choice = ConsoleHelper.promptForInt("Choose An Option");
+            int choice = ConsoleHelper.promptForInt("Choose An Option");
 
-        switch (choice) {
-            case 1 -> addSandwich();
-            case 2 -> addDrink();
-            case 3 -> addChips();
-            case 4 -> checkout();
-            case 0 -> {
-                System.out.println("Your Order Has Been Cancelled");
-                return;
+            switch (choice) {
+                case 1 -> addSandwich();
+                case 2 -> addDrink();
+                case 3 -> addChips();
+                case 4 -> checkout();
+                case 0 -> {
+                    System.out.println("Your Order Has Been Cancelled");
+                    return;
+                }
+                default -> System.out.println("Sorry, Invalid Selection");
             }
-            default -> System.out.println("Sorry, Invalid Selection");
         }
     }
-}
-        //add sandwich
-        //white, wheat, rye, wrap
-        private void addSandwich() {
-            int size = ConsoleHelper.promptForInt("Enter A Size: 4, 8, 12");
-            String breadType = ConsoleHelper.promptForString("Enter Bread Type: White, Wheat, Rye, Wrap");
 
-            Bread bread = new Bread(breadType, 0.0);
-            Sandwich sandwich = new Sandwich("Sandwich", 0.0, bread, size,false);
+    //add sandwich
+    //white, wheat, rye, wrap
+    private void addSandwich() {
+        int size = ConsoleHelper.promptForInt("Enter A Size: 4, 8, 12");
+        String breadType = ConsoleHelper.promptForString("Enter Bread Type: White, Wheat, Rye, Wrap");
 
-            order.addItem(sandwich);
-            System.out.println("Sandwich Added");
-        }
+        String isToasted = ConsoleHelper.promptForString("Would You Like The Sandwich Toasted?(Y/N)").toLowerCase();
 
-        //add drink
-        public void addDrink(){
-        String size = ConsoleHelper.promptForString("Select Drink Size: ");
+        boolean toasted = isToasted.startsWith("y");
+
+        Sandwich sandwich = new Sandwich(new Bread(breadType), size, toasted);
+
+        order.addItem(sandwich);
+        System.out.println("Your sandwhich has been added");
+    }
+
+    //add drink
+    public void addDrink() {
+        String size = ConsoleHelper.promptForString("Select Drink Size: Small/Medium/Large");
+
+        //cherry, orange grape?
         String flavor = ConsoleHelper.promptForString("Select Drink Flavor: ");
 
-        Drinks drink = new Drinks(size, flavor, 0.0);
-            order.addItem(drink);
-            System.out.println("Drink Added");
-        }
+        double price = switch (size.toLowerCase()) {
+            case "small" -> 2.00;
+            case "medium" -> 2.50;
+            case "large" -> 3.00;
+            default -> 2.00;
+        };
 
-        //add chips
+        Drinks drinks = new Drinks(size, flavor);
+        order.addItem(drinks);
 
-        private void addChips(){
-            String type = ConsoleHelper.promptForString("Select A Chip Size");
-            Chips chips = new Chips(type, 1.50);
-            order.addItem(chips);
+        System.out.println("Your drink has been added!");
 
-            System.out.println("Chips Added");
-}
-        private void checkout(){
-            System.out.println(Receipt.generate(order));
-            new ReceiptFileManager().saveReceipt(order);
-            System.out.println("Receipt Saved");
-}
+    }
 }
