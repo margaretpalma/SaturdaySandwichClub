63647913bce915a030d05a63b7a5624f3bf03041
margaretpalma <mpalma1496@gmail.com>
2025-11-10 14:42:04 -0500

    todo for ReceiptFileManager

--- src/main/java/com/saturdaysandwichshop/orders/ReceiptFileManager.java
+++ src/main/java/com/saturdaysandwichshop/orders/ReceiptFileManager.java
@@ -1,15 +1,47 @@
 package com.saturdaysandwichshop.orders;
 
+import java.io.File;
+
 public class ReceiptFileManager {
 
 
-    //implements saving - used for saving the receipts
-    //timestamp for receipts
-    //NEW RECEIPT FOR EACH ORDER
-    //catch exceptions
+    //receipts are stored here //NEW RECEIPT FOR EACH ORDER
+ private static final String Receipt_Folder = "receipts";
+
+     //check for folder
+        public ReceiptFileManager(){
+            File folder = new File(Receipt_Folder);
+
+    //create a new folder for each receipt
+//todo folder for receipts
 
 
 
+
+    //save receipt to file
+            //timestamp for receipts
+
+    //file IN receipt
+
+
+    //catch exception
+    }
 
 
 }
+
+
+
+
+//implements saving - used for saving the receipts
+
+
+
+
+
+
+
+
+
+
+
