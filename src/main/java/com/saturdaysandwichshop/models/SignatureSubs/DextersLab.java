202e9597d2560c69f05d8747ed176e092c239f24
margaretpalma <mpalma1496@gmail.com>
2025-11-13 21:52:00 -0500

    TommyPicklesDeluxe

--- src/main/java/com/saturdaysandwichshop/models/SignatureSubs/DextersLab.java
+++ src/main/java/com/saturdaysandwichshop/models/SignatureSubs/DextersLab.java
@@ -1,6 +1,21 @@
 package com.saturdaysandwichshop.models.SignatureSubs;
 
+import com.saturdaysandwichshop.models.Bread;
 import com.saturdaysandwichshop.models.Sandwich;
+import com.saturdaysandwichshop.models.Toppings;
 
 public class DextersLab extends Sandwich {
+
+    public DextersLab(Bread bread, int size, boolean toasted) {
+        super(new Bread("White"), 8, true);
+
+        //toppings for speciality sandwich
+
+        getToppings().add(new Toppings("Bacon", true,false,2.00));
+        getToppings().add(new Toppings("Cheddar", true,false,1.50));
+        getToppings().add(new Toppings("Lettuce", false,false,0));
+        getToppings().add(new Toppings("Tomatoes", false,false, 0));
+        getToppings().add(new Toppings("Ranch", false,false,0));
+
+    }
 }
---
+++ src/main/java/com/saturdaysandwichshop/models/SignatureSubs/TommyPicklesDeluxe.java
@@ -0,0 +1,16 @@
+package com.saturdaysandwichshop.models.SignatureSubs;
+
+import com.saturdaysandwichshop.models.Bread;
+import com.saturdaysandwichshop.models.Sandwich;
+
+public class TommyPicklesDeluxe extends Sandwich {
+
+    public class TommyPicklesDeluxe(){
+        super(new Bread("White"), 8, true)
+
+
+
+
+
+    }
+}
